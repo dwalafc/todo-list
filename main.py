32c69f5121b4a5576a4b65149b8df11e850cae6f
Erika Bueno <erikab@ciandt.com>
2021-11-30 19:41:34 -0300

    application start

---
+++ main.py
@@ -0,0 +1,45 @@
+# CRUD
+task_list = []
+
+def show_tasks():
+    if len(task_list) == 0:
+        print("Não tenho tarefas!")
+    else:   
+        counter = 1
+        for task in task_list:
+            print("%d - %s" % (counter, task))
+            counter += 1
+
+print("=== OPÇÕES ===")
+print("[A]dicionar")
+print("[V]isualizar todas")
+print("A[t]ualizar")
+print("[E]xcluir")
+print("[S]air")
+print("==============")
+
+while True:
+    op = input("Digite uma opção: ")
+    if op.upper() == "S":
+        break
+    
+    elif op.upper() == "A":
+        task = input("Digite uma tarefa: ")
+        task_list.append(task)
+
+    elif op.upper() == "V":
+        show_tasks()
+    
+    elif op.upper() == "T":
+        show_tasks()
+        position = input("Digite o número da tarefa a ser alterada: ")
+        new_task = input("Digite o novo valor da tarefa: ")
+        task_list[int(position)-1] = new_task
+
+    elif op.upper() == "E":
+        print("Excluir")
+    else:
+        print("Opção inválida!")
+
+print("==============")
+print("Saindo...")
