700f91a7c105a1c9ef2824e2756f06d75361b3f5
Erika Bueno <erikab@ciandt.com>
2021-12-01 19:24:54 -0300

    Refactor

--- main.py
+++ main.py
@@ -1,22 +1,8 @@
-# CRUD
+import utils
+
 task_list = []
 
-def show_tasks():
-    if len(task_list) == 0:
-        print("Não tenho tarefas!")
-    else:   
-        counter = 1
-        for task in task_list:
-            print("%d - %s" % (counter, task))
-            counter += 1
-
-print("=== OPÇÕES ===")
-print("[A]dicionar")
-print("[V]isualizar todas")
-print("A[t]ualizar")
-print("[E]xcluir")
-print("[S]air")
-print("==============")
+utils.menu()
 
 while True:
     op = input("Digite uma opção: ")
@@ -28,18 +14,20 @@ while True:
         task_list.append(task)
 
     elif op.upper() == "V":
-        show_tasks()
+        utils.show_tasks(task_list)
     
     elif op.upper() == "T":
-        show_tasks()
-        position = input("Digite o número da tarefa a ser alterada: ")
-        new_task = input("Digite o novo valor da tarefa: ")
-        task_list[int(position)-1] = new_task
+        utils.show_tasks(task_list)
+        if len(task_list) != 0:
+            position = input("Digite o número da tarefa a ser alterada: ")
+            new_task = input("Digite o novo valor da tarefa: ")
+            task_list[int(position)-1] = new_task
 
     elif op.upper() == "E":
-        show_tasks()
-        position = input("Digite o número da tarefa a ser excluida: ")
-        del task_list[int(position)-1]
+        utils.show_tasks(task_list)
+        if len(task_list) != 0:
+            position = input("Digite o número da tarefa a ser excluida: ")
+            del task_list[int(position)-1]
 
     else:
         print("Opção inválida!")
